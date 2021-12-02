0cf0e99d208e27fec64ca635c8ed23af3cacfd8d
Ty Yoho <tajyoho@gmail.com>
2021-12-01 21:06:33 -0500

    First commit

---
+++ venv/gui.py
@@ -0,0 +1,72 @@
+import PySimpleGUI as sg
+import market_nw as market
+
+#Title Window #####
+title_layout = [    [sg.Text('New World Trading Post'), sg.Button('X')],
+                    [sg.Button('Buy'), sg.Button('Sell')],
+                    [sg.Button('Show Orders')]  ]
+
+title_window = sg.Window('Window Title', title_layout)
+
+#Buy Window #####
+def open_buy():
+    buy_layout = [  [sg.Text('Buy Order'), sg.Button('X')],
+                    [sg.Radio(text='Iron Ore', group_id='item', key='-ore-'), sg.Radio(text='Iron Ingot', group_id='item', key='-ingot-')],
+                    [sg.Text('Total', size=(7,1)), sg.Text('Quantity', size=(8,1))],
+                    [sg.InputText('', size=(8,1), key='-total-'), sg.InputText('', size=(8,1), key='-quan-')],
+                    [sg.Button('Add')]  ]
+    buy_window = sg.Window('Buy Order', buy_layout)
+    event1, value1 = buy_window.read()
+    while True:
+        if event1 == 'X':
+            break
+        if event1 == 'Add':
+            if value1['-ore-']:
+                market.buy_order(name='Iron Ore', total=int(value1['-total-']), quan=int(value1['-quan-']))
+                print('Order placed ', 'iron ore ', value1['-total-'], ' ', value1['-quan-'])
+            if value1['-ingot-']:
+                market.buy_order(name='Iron Ingot', total=int(value1['-total-']), quan=int(value1['-quan-']))
+                print('Order placed ', 'iron ingot ', value1['-total-'], ' ', value1['-quan-'])
+            break
+
+    buy_window.close()
+
+#Sell Window #####
+def open_sell():
+    sell_layout = [[sg.Text('Sell Order'), sg.Button('X')],
+                  [sg.Radio(text='Iron Ore', group_id='item', key='-ore-'),
+                   sg.Radio(text='Iron Ingot', group_id='item', key='-ingot-')],
+                  [sg.Text('Total', size=(7, 1)), sg.Text('Quantity', size=(8, 1))],
+                  [sg.InputText('', size=(8, 1), key='-total-'), sg.InputText('', size=(8, 1), key='-quan-')],
+                  [sg.Button('Sell')]]
+    sell_window = sg.Window('Sell Order', sell_layout)
+    event1, value1 = sell_window.read()
+    while True:
+        if event1 == 'X':
+            break
+        if event1 == 'Sell':
+            if value1['-ore-']:
+                market.sell_order(name='Iron Ore', total=0-int(value1['-total-']), quan=0-int(value1['-quan-']))
+                print('Order placed ', 'iron ore ', 0-int(value1['-total-']), ' ', 0-int(value1['-quan-']))
+            if value1['-ingot-']:
+                market.sell_order(name='Iron Ingot', total=0-int(value1['-total-']), quan=0-int(value1['-quan-']))
+                print('Order placed ', 'iron ingot ', 0-int(value1['-total-']), ' ', 0-int(value1['-quan-']))
+            break
+
+    sell_window.close()
+
+
+# main ###############################################
+while True:
+    event, values = title_window.read()
+    if event == 'Buy':
+        title_window.hide()
+        open_buy()
+        title_window.un_hide()
+    if event == 'Sell':
+        title_window.hide()
+        open_sell()
+        title_window.un_hide()
+    if event == 'X':
+        title_window.close()
+        break
---
+++ venv/market_nw_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_buy_sell_order(self):
+        self.assertEqual(True, False)  # add assertion here
+
+`
+
+if __name__ == '__main__':
+    unittest.main()
